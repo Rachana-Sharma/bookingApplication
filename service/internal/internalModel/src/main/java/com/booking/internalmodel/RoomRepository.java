2883f95a3cb7a624783fb238d98e3a64d48da5e8
Rachana-Sharma <45629361+Rachana-Sharma@users.noreply.github.com>
2020-06-24 20:17:36 +0530

    Add files via upload

--- service/internal/internalModel/src/main/java/com/booking/internalmodel/RoomRepository.java
+++ service/internal/internalModel/src/main/java/com/booking/internalmodel/RoomRepository.java
@@ -1,9 +1,11 @@
 package com.booking.internalmodel;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * The RoomRepository, it extends CrudRepository
@@ -14,5 +16,10 @@ public interface RoomRepository extends CrudRepository<RoomEntity, Integer> {
 	
 	@Query(value="SELECT TOP 1 r.ROOM_ID  FROM ROOM_ENTITY r WHERE r.ROOM_STATUS ='AVAILABLE'  AND r.ROOM_TYPE =:roomType",nativeQuery=true)
 	int findRoom(@Param("roomType")String roomType);
+	
+	@Transactional
+	@Modifying(clearAutomatically = true)
+	@Query("UPDATE RoomEntity r SET r.roomStatus ='AVAILABLE' WHERE r.roomId =:roomId")
+	void updateStatusById(@Param("roomId")int roomId);
 
 }
