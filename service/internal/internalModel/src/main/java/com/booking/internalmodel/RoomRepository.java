9e49bc20a3f5da491f03f7938cb376d517a676b9
Rachana Sharma <45629361+Rachana-Sharma@users.noreply.github.com>
2020-07-03 19:35:42 +0530

    Add files via upload

--- service/internal/internalModel/src/main/java/com/booking/internalmodel/RoomRepository.java
+++ service/internal/internalModel/src/main/java/com/booking/internalmodel/RoomRepository.java
@@ -36,12 +36,28 @@ public interface RoomRepository extends CrudRepository<RoomEntity, Integer> {
 	@Query("UPDATE RoomEntity r SET r.roomStatus ='AVAILABLE' WHERE r.roomId =:roomId")
 	void updateStatusById(@Param("roomId") int roomId);
 
+	/**
+	 * selects room id of the first row that satisfies the given condition and
+	 * allots it for booking
+	 * 
+	 * @param startDate
+	 * @param roomType
+	 * @return room id
+	 */
 	@Query(value = "SELECT TOP 1 ROOM_ENTITY.ROOM_ID FROM ROOM_ENTITY WHERE (ROOM_ENTITY.ROOM_TYPE=:roomType AND ROOM_ENTITY.ROOM_ID IN (SELECT BOOKING_ENTITY.ROOM_ID FROM BOOKING_ENTITY WHERE BOOKING_ENTITY.END_DATE <=:startDate )) OR (ROOM_ENTITY.ROOM_TYPE=:roomType AND ROOM_ENTITY.ROOM_STATUS ='AVAILABLE')", nativeQuery = true)
-	int findRoomByDate(@Param("startDate")Date startDate,@Param("roomType") String roomType);
-	
-	@Query(value = "SELECT TOP 1 ROOM_ENTITY.ROOM_ID FROM ROOM_ENTITY WHERE (ROOM_ENTITY.ROOM_TYPE=:roomType AND ROOM_ENTITY.ROOM_ID NOT IN (SELECT BOOKING_ENTITY.ROOM_ID FROM BOOKING_ENTITY WHERE (BOOKING_ENTITY.START_DATE BETWEEN (:startDate AND :endDate)) OR (BOOKING_ENTITY.END_DATE BETWEEN (:startDate AND :endDate)) OR (BOOKING_ENTITY.START_DATE <=:startDate AND BOOKING_ENTITY.END_DATE >=:endDate))) OR (ROOM_ENTITY.ROOM_TYPE=:roomType AND ROOM_ENTITY.ROOM_STATUS ='AVAILABLE')", nativeQuery = true)
-	int findRoomByDateRange(@Param("startDate")Date startDate,@Param("endDate") Date endDate,@Param("roomType") String roomType);
+	int findRoomByDate(@Param("startDate") Date startDate, @Param("roomType") String roomType);
 
-	
-	
+	/**
+	 * selects room id of the first row that satisfies the given condition and
+	 * allots it for booking
+	 * 
+	 * @param startDate
+	 * @param endDate
+	 * @param roomType
+	 * @return room id
+	 */
+	@Query(value = "SELECT TOP 1 ROOM_ENTITY.ROOM_ID FROM ROOM_ENTITY WHERE (ROOM_ENTITY.ROOM_TYPE=:roomType AND ROOM_ENTITY.ROOM_ID NOT IN (SELECT BOOKING_ENTITY.ROOM_ID FROM BOOKING_ENTITY WHERE (BOOKING_ENTITY.START_DATE BETWEEN (:startDate AND :endDate)) OR (BOOKING_ENTITY.END_DATE BETWEEN (:startDate AND :endDate)) OR (BOOKING_ENTITY.START_DATE <=:startDate AND BOOKING_ENTITY.END_DATE >=:endDate))) OR (ROOM_ENTITY.ROOM_TYPE=:roomType AND ROOM_ENTITY.ROOM_STATUS ='AVAILABLE')", nativeQuery = true)
+	int findRoomByDateRange(@Param("startDate") Date startDate, @Param("endDate") Date endDate,
+			@Param("roomType") String roomType);
+
 }
