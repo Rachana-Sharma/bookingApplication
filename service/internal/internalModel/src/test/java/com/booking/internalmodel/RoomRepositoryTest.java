d9741431433da427b2f4508aa2e2c60778d9e2d8
Rachana Sharma <45629361+Rachana-Sharma@users.noreply.github.com>
2020-07-30 20:38:00 +0530

    Add files via upload

--- service/internal/internalModel/src/test/java/com/booking/internalmodel/RoomRepositoryTest.java
+++ service/internal/internalModel/src/test/java/com/booking/internalmodel/RoomRepositoryTest.java
@@ -1,6 +1,6 @@
-/*package com.booking.internalmodel;
 
-import static org.junit.Assert.assertNotNull;
+package com.booking.internalmodel;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.text.ParseException;
@@ -8,39 +8,26 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//@ExtendWith(SpringExtension.class)
-//@RunWith(SpringRunner.class)
-//@ExtendWith(MockitoExtension.class)
-//@DataJpaTest
-@SpringBootTest
 @RunWith(SpringRunner.class)
-@EnableJpaRepositories(basePackages = {"com.booking.internalmodel"})
-@EntityScan(basePackages = {"com.booking"})
+@ContextConfiguration(locations = { "classpath*:resources/invoices-context.xml",
+		"classpath*:resources/invoices-int-schema.xml" })
 public class RoomRepositoryTest {
 
-	@Autowired
+	@InjectMocks
 	RoomRepository roomRepository;
-	
-//	@Autowired
-//	TestEntityManager entityManager;
-	
+
 	private Date sDate;
-	
+
 	private Date eDate;
+
+	private RoomEntity roomEntity;
 
 	@Test
 	public void findRommByDateTest() throws ParseException {
@@ -49,21 +36,18 @@ public class RoomRepositoryTest {
 		String endDate = "28-07-2020";
 		eDate = new SimpleDateFormat("dd-mm-yyyy").parse(endDate);
 		String roomType = "SINGLE";
-	//	BookingEntity bookingEntity = new BookingEntity();
-	//	bookingEntity.setStartDate(sDate);
-	//	bookingEntity.setEndDate(eDate);
-		RoomEntity roomEntity = new RoomEntity();
+
+		roomEntity = new RoomEntity();
 		roomEntity.setRoomId(1);
 		roomEntity.setRoomPrice(6000);
 		roomEntity.setRoomStatus("AVAILABLE");
 		roomEntity.setRoomType("SINGLE");
-		//roomEntity = entityManager.persistAndFlush(roomEntity);
-		//Integer id = entityManager.persistAndGetId(roomEntity, Integer.class);
-		//assertNotNull(id);
-		//roomRepository.findRoomByDate(sDate, eDate, roomType);
-		roomRepository.save(roomEntity);
-		//assertEquals(e, roomEntity.getRoomId());
-		assertNotNull(roomEntity.getRoomId());
+
+		Mockito.when(
+				roomRepository.findRoomByDate(Mockito.any(Date.class), Mockito.any(Date.class), Mockito.anyString()))
+				.thenReturn(roomEntity.getRoomId());
+		int id = roomRepository.findRoomByDate(Mockito.any(Date.class), Mockito.any(Date.class), Mockito.anyString());
+		assertEquals(1, id);
 	}
-	
-}*/
+
+}
