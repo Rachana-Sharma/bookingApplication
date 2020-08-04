1145483aee7eac290f0897283d22cedde742ee5d
Rachana Sharma <45629361+Rachana-Sharma@users.noreply.github.com>
2020-08-04 20:19:09 +0530

    Add files via upload

--- service/internal/internalClient/src/test/java/com/booking/internalclient/GetRoomByIdClientTest.java
+++ service/internal/internalClient/src/test/java/com/booking/internalclient/GetRoomByIdClientTest.java
@@ -1,40 +1,57 @@
 package com.booking.internalclient;
 
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
-
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Answers;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import com.booking.common.RoomModel;
 
 public class GetRoomByIdClientTest {
 
 	@InjectMocks
-	GetRoomByIdClient getRoomByIdClient;
-	
+	private GetRoomByIdClient getRoomByIdClient = new GetRoomByIdClient();
+
 	@Mock
-	RestTemplate restTemplate;
-	
-	private MockRestServiceServer server;
-	
+	private RestTemplate restTemplate;
+
+	private RoomModel roomModel = null;
+	/**
+	 * baseUrl
+	 */
+	private String baseUrl ="http://localhost:8088" ;
+
+	@BeforeEach
 	public void setUp() {
-		server = MockRestServiceServer.bindTo(restTemplate).build();
+				MockitoAnnotations.initMocks(this);
+		// ReflectionTestUtils.setField(getRoomByIdClient, "baseUrl",
+		// "baseUrl/room/{id}");
+				roomModel = new RoomModel(1, "SINGLE", 5000, "AVAILABLE");
 	}
-	
+
 	@Test
 	public void getRoomByIdClientTest() {
-		
-		//Mockito.when(restTemplate.exchange(Mockito.anyString(),Mockito.<HttpMethod> any(), Mockito.<HttpEntity<?>> any(),Mockito.<Class<?>> any(),Mockito.anyInt())).thenReturn();
-		
-		server.expect(requestTo("/room/1")).andExpect(method(HttpMethod.GET))
-		.andRespond(withSuccess("{\"1\", \"SINGLE\", \"6000\",\"AVAILABLE\"}", MediaType.APPLICATION_JSON));
-		
-		
+
+	
+			Mockito.when(restTemplate.exchange(ArgumentMatchers.eq(baseUrl+"/room/{id}"), ArgumentMatchers.eq(HttpMethod.GET),
+				ArgumentMatchers.any(HttpEntity.class), ArgumentMatchers.eq(RoomModel.class), ArgumentMatchers.eq(1)))
+				.thenReturn(new ResponseEntity<RoomModel>(roomModel, HttpStatus.OK));
+
+			//Mockito.when(restTemplate.exchange(ArgumentMatchers.eq("baseUrl/room/1"), ArgumentMatchers.eq(HttpMethod.GET),
+			//	ArgumentMatchers.any(HttpEntity.class), ArgumentMatchers.eq(RoomModel.class), ArgumentMatchers.eq(1)).getBody())
+			//	.thenReturn(roomModel);
+		RoomModel returnedRoomModel = getRoomByIdClient.getRoomByIdClientMethod(1); 
+		 Assertions.assertEquals( roomModel,RoomModel.class);
 	}
 }
