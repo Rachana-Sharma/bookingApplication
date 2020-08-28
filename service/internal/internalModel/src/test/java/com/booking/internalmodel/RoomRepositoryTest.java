7434fa268c6417860e2a179490ab70ee97c348b9
Rachana Sharma <45629361+Rachana-Sharma@users.noreply.github.com>
2020-08-28 17:26:16 +0530

    Add files via upload

--- service/internal/internalModel/src/test/java/com/booking/internalmodel/RoomRepositoryTest.java
+++ service/internal/internalModel/src/test/java/com/booking/internalmodel/RoomRepositoryTest.java
@@ -5,17 +5,17 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import java.text.ParseException;
 import java.util.Date;
 
-import org.apache.catalina.core.ApplicationContext;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import com.booking.externalservice.DatabaseConfig;
+
+
 
 /**
  * RoomRepositoryTest
@@ -30,11 +30,10 @@ public class RoomRepositoryTest {
 	/**
 	 * defining and initializing RoomRepository object
 	 */
-	@Autowired
+	@MockBean
 	private RoomRepository roomRepository;
 
-	@Autowired
-	private ApplicationContext applicationContext;
+	
 	/**
 	 * the start date
 	 */
@@ -53,8 +52,6 @@ public class RoomRepositoryTest {
 	 * defining and initializing RoomEntity object
 	 */
 	private RoomEntity roomEntity = null;
-	@Autowired
-    private TestEntityManager entityManager;
 
 	/**
 	 * sets up all the values and is executed before the actual test cases runs
@@ -89,10 +86,9 @@ public class RoomRepositoryTest {
 		roomEntity.setRoomPrice(6000);
 		roomEntity.setRoomStatus("AVAILABLE");
 		roomEntity.setRoomType("SINGLE");
-		this.entityManager.persist(roomEntity);
 		// int id = roomRepository.findRoomByDate(Mockito.any(Date.class),
 		// Mockito.any(Date.class), Mockito.anyString());
-		int id1 = this.roomRepository.findRoomByDate(sDate, eDate, roomType);
+		int id1 = roomRepository.findRoomByDate(sDate, eDate, roomType);
 		assertNotNull(id1);
 	}
 }
